4498f904f82e549941bae57681c292caa871041a
sahib sadiyev <sahibsadiyev@sahibs-iMac.local>
2026-03-30 19:14:49 +0400

    Add full Xcode project

---
+++ notepadTests/notepadTests.swift
@@ -0,0 +1,17 @@
+//
+//  notepadTests.swift
+//  notepadTests
+//
+//  Created by sahib sadiyev on 29.03.26.
+//
+
+import Testing
+@testable import notepad
+
+struct notepadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ notepadUITests/notepadUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  notepadUITestsLaunchTests.swift
+//  notepadUITests
+//
+//  Created by sahib sadiyev on 29.03.26.
+//
+
+import XCTest
+
+final class notepadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
